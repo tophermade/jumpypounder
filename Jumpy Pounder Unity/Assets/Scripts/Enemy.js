5361e15a864ca7a9aaa87d40e42eb68c14d3d551
Topher <tophermade@gmail.com>
2016-02-16 07:52:34 -0700

    initial

---
+++ Jumpy Pounder Unity/Assets/Scripts/Enemy.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var goRight 		: boolean 		= true;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if(goRight){
+		GetComponent(Rigidbody2D).velocity = Vector2(3, 0);
+	} else {
+		GetComponent(Rigidbody2D).velocity = Vector2(-3, 0);
+	}
+
+}
---
+++ Jumpy Pounder Unity/Assets/Scripts/Player.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+
+var playing 		: boolean 		= true;
+var canJump 		: boolean 		= true;
+var inAir 			: boolean 		= false;
+
+
+
+function OnCollisionEnter2D(coll: Collision2D) {
+	if (coll.transform.tag == "Enemy"){
+		print("Hit Enemey");
+		for (var theHit: ContactPoint2D in coll.contacts) {
+			var hitPoint: Vector2 = theHit.point;
+			//Instantiate(explosion, new Vector3(hitPoint.x, hitPoint.y, 0), Quaternion.identity);
+		}
+	} else if(coll.transform.tag == "Ground"){
+		canJump = true;
+		inAir = false;
+		GetComponent(Rigidbody2D).velocity = Vector2(0, 6);
+	}
+}
+
+
+
+function Start () {
+
+}
+
+
+
+function Update () {
+	if(Input.GetMouseButtonDown(0) && playing){
+		if(canJump){
+			print("tapped");
+			canJump = false;
+			inAir = true;
+			GetComponent(Rigidbody2D).velocity = Vector2(0, 20);
+		} else if(!canJump && inAir){			
+			GetComponent(Rigidbody2D).velocity = Vector2(0, -16);
+		}
+	}// mouse down & playing
+}
